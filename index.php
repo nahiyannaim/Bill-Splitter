ec845122ba8c3bd1f1d8f6f1101b60bd788159f2
Nahiyan Naim <nahiyannaim@yahoo.com>
2019-06-15 04:26:17 -0700

    Added initial template

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+    // Do backend calculation
+?>
+
 <!DOCTYPE html>
 
 <html lang="en">
@@ -13,9 +17,15 @@
 
 <body>
 
-    <?php
-        echo "<p> Hello!</p>";
-    ?>
+    <h3>Total Bill $: </h3>
+
+    <h3>Number of People: </h3>
+
+    <h3>Tax Percentage: </h3>
+
+    <h3>Tip % / $: </h3>
+
+    <button> Split </button>
 
 </body>
 
